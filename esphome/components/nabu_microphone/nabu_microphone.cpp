d4f28e1ce240af277e51c8118ec71d364d1ea7d4
Nikolay Nikolaev <nikolay.nikolaev@living.homes>
2025-02-24 16:52:02 +0200

    test

--- esphome/components/nabu_microphone/nabu_microphone.cpp
+++ esphome/components/nabu_microphone/nabu_microphone.cpp
@@ -13,7 +13,7 @@
 #include "esphome/components/ota/ota_backend.h"
 #endif
 
-#include "MQTTClient.h"
+#include "paho_mqtt_c/MQTTClient.h"
 
 #define MQTT_BROKER    "tcp://broker.hivemq.com:1883"
 #define CLIENT_ID      "PahoEmbeddedCClient"
@@ -115,15 +115,15 @@ void NabuMicrophone::setup() {
       });
 #endif
 
-  Network network;
-  MQTTClient client;
-  unsigned char sendbuf[100], readbuf[100];
+  // Network network;
+  // MQTTClient client;
+  // unsigned char sendbuf[100], readbuf[100];
 
-  // Initialize network and MQTT client
-  NetworkInit(&network);
-  NetworkConnect(&network, "broker.hivemq.com", 1883);
+  // // Initialize network and MQTT client
+  // NetworkInit(&network);
+  // NetworkConnect(&network, "broker.hivemq.com", 1883);
 
-  MQTTClientInit(&client, &network, TIMEOUT, sendbuf, sizeof(sendbuf), readbuf, sizeof(readbuf));
+  // MQTTClientInit(&client, &network, TIMEOUT, sendbuf, sizeof(sendbuf), readbuf, sizeof(readbuf));
 
 }
 
